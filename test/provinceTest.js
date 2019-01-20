f34f9be17ac8034e1114e229fb4f7d854bf772fc
Ian Kim <iankim@13mile.co.kr>
2019-01-21 08:24:21 +0900

    first test

---
+++ test/provinceTest.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+import Province from '../src/Province'
+
+
+describe('province', function () {
+    it('shortfall', function () {
+        let asia = new Province(sampleProvinceData());
+        expect(asia.shortfall).equal(5);
+    });
+});
+
+
+function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+            {name: "Attalia", cost: 12, production: 10},
+            {name: "Sinope", cost: 10, production: 6},
+        ],
+        demand: 30,
+        price: 20
+    };
+}
